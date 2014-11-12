2a8de43a3a0466d263aaece4eec8c2d8a1e6a8af
Damien Dart <damiendart@pobox.com>
2014-11-12 22:42:13 +0000

    Perform some minor reformatting.

--- src/brainfuck.h
+++ src/brainfuck.h
@@ -1,9 +1,9 @@
 /*
  * A simple brainfuck intepreter library.
  *
- * This file was written by Damien Dart, <damiendart@pobox.com>. This is free
- * and unencumbered software released into the public domain. For more
- * information, please refer to the accompanying "UNLICENCE" file.
+ * This file was written by Damien Dart, <damiendart@pobox.com>. This is
+ * free and unencumbered software released into the public domain. For
+ * more information, please refer to the accompanying "UNLICENCE" file.
 */
 
 #ifndef BRAINFUCK_H
--- src/interpreter.c
+++ src/interpreter.c
@@ -1,9 +1,9 @@
 /*
  * A simple brainfuck interpreter.
  *
- * This file was written by Damien Dart, <damiendart@pobox.com>. This is free
- * and unencumbered software released into the public domain. For more
- * information, please refer to the accompanying "UNLICENCE" file.
+ * This file was written by Damien Dart, <damiendart@pobox.com>. This is
+ * free and unencumbered software released into the public domain. For
+ * more information, please refer to the accompanying "UNLICENCE" file.
 */
 
 #include <errno.h>
